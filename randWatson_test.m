224b0074a4ccd4fbeda401abb7e241810f7f792f
yuhuic <yuhuic@umich.edu>
2015-08-05 19:36:23 -0400

    Modified the testcase to generate cover photo on MATLAB central.

--- randWatson_test.m
+++ randWatson_test.m
@@ -25,12 +25,20 @@ figure;
 scatter3(RandWatson(:,1), RandWatson(:,2), RandWatson(:,3),3, 'b', 'filled'), axis([-1 1 -1 1 -1 1]);
 
 
-
+% Generate the cover image of MATLAB central.
 clear;
-mu = [0,0,1];
-k=-35;
+mu = [1,-1,-1];
+mu = mu / norm(mu);
+k=-15;
 N=1000;
 [RandWatson] = randWatson(N, mu, k);
 figure; 
 scatter3(RandWatson(:,1), RandWatson(:,2), RandWatson(:,3),3, 'b', 'filled'), axis([-1 1 -1 1 -1 1]);
+mu = [1,-1,-1];
+mu = mu / norm(mu);
+k=30;
+N=1000;
+[RandWatson] = randWatson(N, mu, k);
+hold on;
+scatter3(RandWatson(:,1), RandWatson(:,2), RandWatson(:,3),3, 'r', 'filled'), axis([-1 1 -1 1 -1 1]);
 
